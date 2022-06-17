15492ba3957da7e141836c93b99f014c1ef04d5c
Alejandro Rua <alejoruasuarez@gmail.com>
2022-06-17 13:50:26 -0500

    Folder structure for go project

---
+++ apps/stock/adapter.go
@@ -0,0 +1,13 @@
+package stock
+
+import (
+	"fmt"
+)
+
+func StockDoSomething() {
+	fmt.Println("stock adapter...")
+
+	//p := domain.Product{Name: "Iphone 14", Price: 800.99}
+
+	//fmt.Println("product name:", p.Name)
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "marketplace/apps/stock"
+
+func main() {
+	stock.StockDoSomething()
+	/*testPage()
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/album/:id", getAlbumById)
+	router.GET("/reverse/:text", reverse)
+	router.POST("/albums", postAlbum)
+	router.Run("localhost:3000")*/
+}
